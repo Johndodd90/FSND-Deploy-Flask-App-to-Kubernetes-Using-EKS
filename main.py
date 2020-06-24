5ca863e3ae3c897065519e49a2e096d4589f6301
John Dodd <johndodd90@gmail.com>
2020-06-23 20:17:39 -0500

    Dockerfile added

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def _logger():
 
     RETURNS: log object
     '''
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     log = logging.getLogger(__name__)
     log.setLevel(LOG_LEVEL)
@@ -34,8 +35,9 @@ def _logger():
 
 
 LOG = _logger()
-LOG.debug("Starting with log level: %s" % LOG_LEVEL )
+LOG.debug("Starting with log level: %s" % LOG_LEVEL)
 APP = Flask(__name__)
+
 
 def require_jwt(function):
     """
@@ -49,7 +51,7 @@ def require_jwt(function):
         token = str.replace(str(data), 'Bearer ', '')
         try:
             jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-        except: # pylint: disable=bare-except
+        except:  # pylint: disable=bare-except
             abort(401)
 
         return function(*args, **kws)
@@ -68,7 +70,9 @@ def auth():
     """
     request_data = request.get_json()
     email = request_data.get('email')
+    print(email)
     password = request_data.get('password')
+    print(password)
     if not email:
         LOG.error("No email provided")
         return jsonify({"message": "Missing parameter: email"}, 400)
@@ -78,6 +82,8 @@ def auth():
     body = {'email': email, 'password': password}
 
     user_data = body
+    token = _get_jwt(user_data).decode('utf-8')
+    print(token)
 
     return jsonify(token=_get_jwt(user_data).decode('utf-8'))
 
@@ -87,19 +93,21 @@ def decode_jwt():
     """
     Check user token and return non-secret data
     """
+
     if not 'Authorization' in request.headers:
         abort(401)
     data = request.headers['Authorization']
     token = str.replace(str(data), 'Bearer ', '')
+
     try:
         data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except: # pylint: disable=bare-except
-        abort(401)
 
+    except:  # pylint: disable=bare-except
+        abort(401)
 
     response = {'email': data['email'],
                 'exp': data['exp'],
-                'nbf': data['nbf'] }
+                'nbf': data['nbf']}
     return jsonify(**response)
 
 
@@ -110,5 +118,7 @@ def _get_jwt(user_data):
                'email': user_data['email']}
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
 
+
 if __name__ == '__main__':
-    APP.run(host='127.0.0.1', port=8080, debug=True)
+    APP.run(host='127.0.0.1', port=8000, debug=True)
+    # Changed to port to 8000 from 8080 as there was an OS error
